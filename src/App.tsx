1355dd3a19b213bf133b5968bbab029621fbadaa
TeymurDev <teymur.ff@gmail.com>
2026-07-15 07:36:42 +0400

    refactor: remove unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,12 @@ import { Icon } from './components/Icon';
 
 function App() {
   return (
-    <>
-      <div class='flex items-center justify-center gap-4'>
-        <Icon name='Heart' size='md' color='success' />
-        <Icon name='Settings' size='lg' color='muted' />
-        <Icon name='Settings' />
-        <Icon name='Heart' size='md' customSize={100} color='error' />{' '}
-      </div>
-    </>
+    <div class='flex items-center justify-center gap-4'>
+      <Icon name='Heart' size='md' color='success' />
+      <Icon name='Settings' size='lg' color='muted' />
+      <Icon name='Settings' />
+      <Icon name='Heart' size='md' customSize={100} color='error' />{' '}
+    </div>
   );
 }
 
